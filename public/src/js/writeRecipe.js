13e0e80c6b2c916c220312393d979b8ae04935a7
Chae Eun Lee <nuguziii@naver.com>
2019-12-04 18:05:42 +0900

    writeRecipe 상품 추가, 삭제 버튼 js 작성 및 API 완료

--- public/src/js/writeRecipe.js
+++ public/src/js/writeRecipe.js
@@ -1,21 +1,65 @@
-document.querySelector('.showWrap').addEventListener('click', async function (e) {
-    var data = {};
-    var title = document.querySelector('rcp_title').value;
-    var tagcontentsAll = document.querySelectorAll('#rcp_product'); //#xxx#xxx;
-    var tagcontents = '';
-    for(var i=0;i<tagcontentsAll.length;i++){
-        tagcontents += '#'+tagcontentsAll[i].value;
-    }
-    var difficulty = ''; //mid/low/high 중 하나
-    var totaltime = document.querySelector('#rcp_time').value;
-    var videoUrl = '';
-    var content1 = '';
-    var content2 = '';
-    var content3 = '';
-    var content4 = '';
-    var content5 = '';
+import honeycomboAPI from "../apis/honeycomboAPI.js";
 
-    data.title = title;
-    data.tagcontents = tagcontents;
-    
+var adds = document.querySelectorAll('#add');
+
+[].forEach.call(adds, function(add){
+    add.addEventListener("click", addClick, false);
 });
+function addClick(e) {
+    let itemId = "item" + e.srcElement.name;
+    let itemInfos = document.querySelectorAll("#" + itemId);
+    let title = itemInfos[1].textContent;
+    let imgUrl = itemInfos[2].src;
+
+    let currentItemHTML = document.querySelector("#myItemList").innerHTML;
+    document.querySelector("#myItemList").innerHTML = currentItemHTML +
+        '<div class="card align-self-center ml-4">\
+            <div class="card-body text-center">\
+                <input type="button" name='+itemId+' id="delete" onclick="delClick(name)" class="btn btn-primary d-block" value="x" />\
+                <h6 class="card-title" id="rcp_product">' + title + '</h6>\
+                <img class="rounded mx-auto d-block" width=100 height=100 src=' + imgUrl + '>\
+            </div>\
+        </div>';
+}
+
+function delClick(name){
+    let delButton = document.querySelector('input[name='+name+']');
+    let allPath = delButton.parentNode.parentNode;
+    allPath.remove();
+}
+
+document.querySelector('#write_submit').addEventListener('click', async function (e) {
+
+    var data = {};
+    data.title = document.querySelector('#rcp_title').value;
+    var tagcontentsAll = document.querySelectorAll('#rcp_product'); //#xxx#xxx;
+    data.tagcontents = '';
+    for(var i=0;i<tagcontentsAll.length;i++){
+        data.tagcontents += '#'+tagcontentsAll[i].textContent;
+    }
+
+    try{
+        data.difficulty = document.querySelector(".diff.active").id;
+        data.imgUrl = document.querySelector('#preview').src;
+        data.totaltime = document.querySelector('#rcp_time').value;
+        data.videoUrl = document.querySelector('#youtube_url').value;
+        data.content1 = document.querySelector('#step1').value;
+        data.content2 = document.querySelector('#step2').value;
+        data.content3 = document.querySelector('#step3').value;
+        data.content4 = document.querySelector('#step4').value;
+        data.content5 = document.querySelector('#step5').value;
+
+        if(totaltime=="" || videoUrl=="" || content1=="" || title==""){
+            alert("빈칸을 채워주세요!")
+        }else{
+            console.log(data);
+            await honeycomboAPI.postUserRecipe(data);
+        }
+    }catch(e){
+        alert("빈칸을 채워주세요!");
+    }
+});
+
+async function addButton(){
+    console.log("click");
+}
